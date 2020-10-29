c1939f0d9e447ef5dbb72f2e499a38ffd9855979
Luis E. Dalmau <lukedalmau@gmail.com>
2020-10-29 18:35:19 -0400

    algunos arreglos en los input de año

--- project/chat/filters.py
+++ project/chat/filters.py
@@ -1,8 +1,10 @@
 import django_filters as dfil
 from project.models import *
-
+from django import forms
+import datetime
 class ChatFilter(dfil.FilterSet):
-    Date = dfil.DateRangeFilter()
+    year = datetime.date.today().year
+    Date = dfil.DateRangeFilter(widget=forms.SelectDateWidget(years=range(2015,year)))
     type = dfil.CharFilter('type',widget=dfil.widgets.forms.widgets.Select(choices=(('','--------'),('I','Important'),('W','Warning'),('N','Normal'))))
     class Meta:
         model = Chat
