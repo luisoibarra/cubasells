767305fcc7d09befb906646615b1882fe3cef985
Luis E. Dalmau <lukedalmau@gmail.com>
2020-11-05 02:35:03 -0500

    Deploy 1.1

--- project/store/filters.py
+++ project/store/filters.py
@@ -7,12 +7,12 @@ class StoreFilter(dfil.FilterSet):
     year = datetime.date.today().year
     create_date_gte =dfil.DateFromToRangeFilter(
         field_name='Create_Date',lookup_expr='gte',widget=forms.SelectDateWidget(
-            years=range(2015,year+50)
+            years=range(2019,year)
             )
         )
     create_date_lte = dfil.DateFromToRangeFilter(
         field_name='Create_Date', lookup_expr='lte', widget=forms.SelectDateWidget(
-            years=range(2015, year+50)
+            years=range(2019, year)
             )
         )
     class Meta:
