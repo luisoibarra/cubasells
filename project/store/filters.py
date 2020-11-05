e8d014d384c1eede8b6e1e838632fbdba4d2d3ff
Luiso <luise98cu@gamil.com>
2020-11-05 07:06:38 -0500

    DateTime widgets added to filters and forms

--- project/store/filters.py
+++ project/store/filters.py
@@ -4,13 +4,13 @@ from django import forms
 import datetime
 
 class StoreFilter(dfil.FilterSet):
-    year = datetime.date.today().year
-    create_date_gte =dfil.DateFromToRangeFilter(
+    year = datetime.date.today().year+1
+    create_date_gte =dfil.DateTimeFilter(
         field_name='Create_Date',lookup_expr='gte',widget=forms.SelectDateWidget(
             years=range(2019,year)
             )
         )
-    create_date_lte = dfil.DateFromToRangeFilter(
+    create_date_lte = dfil.DateTimeFilter(
         field_name='Create_Date', lookup_expr='lte', widget=forms.SelectDateWidget(
             years=range(2019, year)
             )
